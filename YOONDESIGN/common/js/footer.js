723a8fe0ed8ca4bef0d1f8d5bf72622ec9c055ac
2taeyoon <2tae@kakao.com>
2023-05-18 02:04:31 +0900

    fhgfdhdfh

---
+++ YOONDESIGN/common/js/footer.js
@@ -0,0 +1,8 @@
+// footer start!
+const familySite = document.querySelector('.family_site');
+const familySiteUl = document.querySelector('.family_site > ul');
+
+familySite.addEventListener('click', () => {
+    familySiteUl.classList.toggle('active');
+});
+// footer end!
